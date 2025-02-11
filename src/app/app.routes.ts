1fa120909825b92aa8ee797cfe6516ac178d5d23
Giulio <giuliosignox@gmail.com>
2025-02-11 11:58:34 +0100

    commit2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { VassoioComponent } from './components/vassoio/vassoio.component';
-
-export const routes: Routes = [
-    { path: 'vassoio', component: VassoioComponent },
-=======
 import { WelcomeComponent } from './components/welcome/welcome.component';
 
 export const routes: Routes = [
+    { path: 'vassoio', component: VassoioComponent },
     { path: '', component:WelcomeComponent }
->>>>>>> origin/master
 ];
