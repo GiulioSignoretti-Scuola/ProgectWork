df51c9116732ccaa382d99a10880ba7552163770
Francesco Agarri <francesco.agarri@gmail.com>
2025-02-11 10:08:34 +0100

    Aggiunta drei Prodotti

---
+++ src/app/components/prodotto/prodotto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodotto',
+  standalone: true,
+  imports: [],
+  templateUrl: './prodotto.component.html',
+  styleUrl: './prodotto.component.css'
+})
+export class Prodotto {
+  
+}
